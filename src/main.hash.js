988592aad9ca42dc2b682dd7cd5fafde6bb36a8c
Gyeongmin Kim <gmkim716@gmail.com>
2025-03-03 16:32:30 +0900

    feat: hashRouter 테스트를 통과시킵니다

--- src/main.hash.js
+++ src/main.hash.js
@@ -1,1 +1,37 @@
 import "./main.js";
+import { router } from "../router.js";
+import { createHashRouter } from "./lib/createHashRouter.js";
+import { HomePage } from "./pages/HomePage.js";
+import { globalStore } from "../stores/index.js";
+import { ForbiddenError, UnauthorizedError } from "./errors/index.js";
+import { LoginPage, ProfilePage } from "./pages/index.js";
+import { render } from "./render.js";
+
+router.set(
+  createHashRouter({
+    "/": HomePage,
+    "/login": () => {
+      const { loggedIn } = globalStore.getState();
+      if (loggedIn) {
+        throw new ForbiddenError();
+      }
+      return LoginPage();
+    },
+    "/profile": () => {
+      const { loggedIn } = globalStore.getState();
+      if (!loggedIn) {
+        throw new UnauthorizedError();
+      }
+      return ProfilePage();
+    },
+  }),
+);
+
+function main() {
+  router.get().subscribe(render);
+  globalStore.subscribe(render);
+
+  render();
+}
+
+main();
